044f2e5b34a3f0bf6d0ef7b55b55e07e4eae2224
Semyon Zasov <semenmaslina@gmail.com>
2022-08-11 17:25:25 +0300

    сделал кастомный скролл

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -4,27 +4,27 @@ import styles from "./table.module.scss";
 export const Table: FC = () => {
   return (
     <>
-    <div className={styles.t1}>
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <td width="5%">#</td>
-            <td>name</td>
-            <td>point</td>
-            <td>miss</td>
-            <td>date</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className={styles.column}>
-            <td>1</td>
-            <td>shiroyam</td>
-            <td>321</td>
-            <td>323</td>
-            <td>14.04.2021</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className={styles.t1}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <td width="5%">#</td>
+              <td>name</td>
+              <td>point</td>
+              <td>miss</td>
+              <td>date</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className={styles.column}>
+              <td>1</td>
+              <td>shiroyam</td>
+              <td>321</td>
+              <td>323</td>
+              <td>14.04.2021</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   );
